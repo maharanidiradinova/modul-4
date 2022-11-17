44b66080198e38bfbf514476b499e109c71f4442
maharanidiradinova <114593041+maharanidiradinova@users.noreply.github.com>
2022-11-18 01:44:32 +0700

    Add files via upload

---
+++ lat11.java
@@ -0,0 +1,16 @@
+package Maharani2101092052_modul4;
+
+public class lat11 {
+    public static void main(String[] args) {
+        int number = 10;
+        char letter;
+        letter = 'a';
+        boolean result = true;
+        String str = "hello";
+        
+        System.out.println("number = " +number);
+        System.out.println("Letter = " + letter);
+        System.out.println("result = " + result);
+        System.out.println("String = " + str);
+    } 
+}
